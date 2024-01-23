fc7e9a34fb2a646c0d4e87009a3ce0e524ba184e
FMawsor <frankymawsor11@gmail.com>
2024-01-23 20:07:34 +0530

    commit message

---
+++ web/test7.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <div class="cnotain">
+        <div class="head">
+            <h1>Details of The Employee Salary</h1></div>
+</div>
+<form method="POST">
+    Enter the Salary: <textarea name="n1" cols="30" rows="20">
+</textarea><br><br>
+<input type="submit" value="Submit" name="submit"/>&nbsp;
+<input type="reset" value="Reset" />
+</form>
+</body>
+</html>
